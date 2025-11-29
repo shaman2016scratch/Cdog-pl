8477ede765cadbab548b6c10a44d3156df6b0a13
Shaman2016 scratcher <g.ilkh@yandex.ru>
2025-11-29 22:08:32 +0300

    Update 1.0.js

--- C@/ECMAScript/1.0.js
+++ C@/ECMAScript/1.0.js
@@ -1,3 +1,19 @@
 // C@ code
 // Created by SHAMAN2016
 // License: MIT
+Commands = []; UserCommands = {};
+Cdog = {}
+Cdog_config = {}
+function Run_Cdog(c) {
+  Cdog.code = c.split("^;")
+  for(Cdog.i = 0; Cdog.i < Cdog.code.length; Cdog.i++) {
+    Cdog.i2 = Cdog.code[Cdog.code[Cdog.i]]
+    if (Cdog.i2 === 'let system()') {
+      Cdog.i++; Cdog.system.code = Cdog.i2.split("&^")
+    } else if (Cdog.i2 === 'let main()') {
+      Cdog.i++; Cdog.main.code = Cdog.i2.split(";")
+    } else if (Cdog.i2.split[" "][0] === '#include' || Cdog.i2.split[" "][0] === '#import' || Cdog.i2.split[" "][0] === 'import') {
+      Cdog.import.code = Cdog.i2.split(" ")
+    }
+  }
+}
