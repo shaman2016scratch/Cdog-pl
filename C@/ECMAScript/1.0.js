c6bd52a90bc2da455d2c7fd63c45ed2a1c1edaf8
Shaman2016 scratcher <g.ilkh@yandex.ru>
2025-11-30 13:56:12 +0300

    Update 1.0.js

--- C@/ECMAScript/1.0.js
+++ C@/ECMAScript/1.0.js
@@ -17,6 +17,22 @@ function Run_Cdog(c) {
     Cdog.i2 = Cdog.code[Cdog.i]
     if (Cdog.i2 === 'let system()') {
       Cdog.i++; Cdog.system.code = Cdog.i2.split("&^")
+      for(Cdog.system.i = 0; Cdog.system.i < Cdog.system.code; Cdog.system.i++) {
+        Cdog.system.i2 = Cdog.system.code[Cdog.i]
+        if (Cdog.system.i2 === '#user') {
+          Cdog_config.user.name = Cdog.system.code[Cdog.i+1]
+          Cdog_config.user.agent = Cdog.system.code[Cdog.i+2]
+          Cdog_config.user.pass = Cdog.system.code[Cdog.i+3]
+          Cdog.system.i = Cdog.system.i+3
+        } else if (Cdog.system.i2 === '#program') {
+          Cdog_config.program.name = Cdog.system.code[Cdog.i+1]
+          Cdog_config.program.agent = Cdog.system.code[Cdog.i+2]
+          Cdog_config.program.description = Cdog.system.code[Cdog.i+3]
+          Cdog_config.program.creators = Cdog.system.code[Cdog.i+4]
+          Cdog_config.program.version = Cdog.system.code[Cdog.i+5]
+          Cdog.system.i = Cdog.system.i+5
+        }
+      }
     } else if (Cdog.i2 === 'let main()') {
       Cdog.i++; Cdog.main.code = Cdog.i2.split(";")
     } else if (Cdog.i2.split(" ")[0] === '#include' || Cdog.i2.split(" ")[0] === '#import' || Cdog.i2.split(" ")[0] === 'import') {
