1c59f6b7ffadecba07a745c6fb332747cedc44db
Shaman2016 scratcher <g.ilkh@yandex.ru>
2025-11-30 13:37:11 +0300

    Update 1.0.js

--- C@/ECMAScript/1.0.js
+++ C@/ECMAScript/1.0.js
@@ -14,7 +14,7 @@ Cdog_data = {
 function Run_Cdog(c) {
   Cdog.code = c.split("^;")
   for(Cdog.i = 0; Cdog.i < Cdog.code.length; Cdog.i++) {
-    Cdog.i2 = Cdog.code[Cdog.code[Cdog.i]]
+    Cdog.i2 = Cdog.code[Cdog.i]
     if (Cdog.i2 === 'let system()') {
       Cdog.i++; Cdog.system.code = Cdog.i2.split("&^")
     } else if (Cdog.i2 === 'let main()') {
