da71a3b03e63747a018dd0174a621a5fcdef7656
Shaman2016 scratcher <g.ilkh@yandex.ru>
2025-11-30 10:38:12 +0300

    Update 1.0.js

--- C@/ECMAScript/1.0.js
+++ C@/ECMAScript/1.0.js
@@ -3,7 +3,14 @@
 // License: MIT
 Commands = []; UserCommands = {};
 Cdog = {}
-Cdog_config = {}
+Cdog_config = {
+  "user": {},
+  "program": {}
+}
+Cdog_data = {
+  "libs": {},
+  "src": {}
+}
 function Run_Cdog(c) {
   Cdog.code = c.split("^;")
   for(Cdog.i = 0; Cdog.i < Cdog.code.length; Cdog.i++) {
@@ -14,6 +21,12 @@ function Run_Cdog(c) {
       Cdog.i++; Cdog.main.code = Cdog.i2.split(";")
     } else if (Cdog.i2.split[" "][0] === '#include' || Cdog.i2.split[" "][0] === '#import' || Cdog.i2.split[" "][0] === 'import') {
       Cdog.import.code = Cdog.i2.split(" ")
+      if (Cdog_data.src[Cdog.import.code[1]]) {
+        lib_Cdog = Cdog_data.src[Cdog.import.code[1]]
+        Cdog_data.libs[lib_Cdog['data.json'].sod.name] = lib_Cdog
+      } else {
+        console.error('File not found')
+      }
     }
   }
 }
